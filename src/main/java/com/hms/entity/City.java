b44c339409cee80e18da85d6636baa0ba8693712
vishalrajchaurasia <vchaurasia7@gmail.com>
2025-01-30 23:56:34 +0530

    We have add City.java in entity section and add some data id and name

---
+++ src/main/java/com/hms/entity/City.java
@@ -0,0 +1,35 @@
+package com.hms.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "city")
+public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
