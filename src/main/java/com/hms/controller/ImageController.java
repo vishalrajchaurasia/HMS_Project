fdea089e32b8a9d9669952899a01f69ddccdd950
vishalrajchaurasia <vchaurasia7@gmail.com>
2025-02-02 00:54:25 +0530

    feat: Integrate AWS S3 for file uploads - Configured AWS S3 in `AWSS3Config` to enable file storage. - Implemented `BucketService` to handle file uploads to S3. - Created `ImageController` to expose an API for image uploads. - Added AWS credentials and region properties in `application.properties`. - Included AWS SDK dependency in `pom.xml`.
    This integration allows secure file uploads to AWS S3, improving media storage and accessibility.

---
+++ src/main/java/com/hms/controller/ImageController.java
@@ -0,0 +1,31 @@
+package com.hms.controller;
+
+import com.hms.entity.AppUser;
+import com.hms.service.BucketService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/v1/images")
+public class ImageController {
+
+    private BucketService bucketService;
+
+    public ImageController(BucketService bucketService) {
+        this.bucketService = bucketService;
+    }
+
+    @PostMapping(path = "/upload/file/{bucketName}/property/{propertyId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> uploadFile(@RequestParam MultipartFile file,
+                                        @PathVariable String bucketName,
+                                        @PathVariable long propertyId,
+                                        @AuthenticationPrincipal AppUser user
+    ) {
+        String imageUrl = bucketService.uploadFile(file, bucketName);
+        return new ResponseEntity<>(imageUrl, HttpStatus.OK);
+    }
+}
