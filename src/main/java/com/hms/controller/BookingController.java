661c4bd6e2b60fefa82fedf4cb936e1be6594330
vishalrajchaurasia <vchaurasia7@gmail.com>
2025-02-03 01:41:53 +0530

    feat: Add PDF generation for booking details - Implemented PDFService to generate booking PDFs using iText - Added BookingController to handle booking creation API - Configured iText dependency in pom.xml - Generates a simple PDF with booking details

--- src/main/java/com/hms/controller/BookingController.java
+++ src/main/java/com/hms/controller/BookingController.java
@@ -1,18 +1,34 @@
 package com.hms.controller;
 
+import com.hms.entity.Bookings;
+import com.hms.entity.Property;
+import com.hms.repository.BookingsRepository;
+import com.hms.repository.PropertyRepository;
 import com.hms.service.PDFService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/bookings")
 public class BookingController {
     private PDFService pdfService;
+    private PropertyRepository propertyRepository;
+    private BookingsRepository bookingsRepository;
+
+    public BookingController(PDFService pdfService, PropertyRepository propertyRepository, BookingsRepository bookingsRepository) {
+        this.pdfService = pdfService;
+        this.propertyRepository = propertyRepository;
+        this.bookingsRepository = bookingsRepository;
+    }
 
     @PostMapping("/create-booking")
-    public String createBookings( ){
-        pdfService.generateBookingPdf("G:\\hms_bookings\\test.pdf");
+    public String createBookings(
+           @RequestParam long propertyId,
+           @RequestBody Bookings booking
+    ){
+        Property property = propertyRepository.findById(propertyId).get();
+        Bookings savedBooking = bookingsRepository.save(booking);
+
+        pdfService.generateBookingPdf("G:\\hms_bookings\\confirmation-order"+savedBooking.getId()+".pdf",property);
         return "Booking Created successfully.";
     }
 }
--- src/main/java/com/hms/service/PDFService.java
+++ src/main/java/com/hms/service/PDFService.java
@@ -1,7 +1,7 @@
 package com.hms.service;
 
+import com.hms.entity.Property;
 import com.itextpdf.text.*;
-import com.itextpdf.text.pdf.PdfDocument;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.stereotype.Service;
@@ -10,14 +10,21 @@ import java.io.FileOutputStream;
 
 @Service
 public class PDFService {
-    public void generateBookingPdf(String filePath){
+    public void generateBookingPdf(String filePath, Property property){
        try {
            Document document = new Document();
            PdfWriter.getInstance(document, new FileOutputStream(filePath));
            document.open();
            PdfPTable table = new PdfPTable(2);
-           table.addCell("hi");
-           table.addCell("hi");
+
+           table.addCell("Id");
+           table.addCell(property.getId().toString());
+           table.addCell("Name");
+           table.addCell(property.getName());
+           table.addCell("no of guest");
+           table.addCell(property.getNo_of_guest().toString());
+
+
            document.add(table);
            document.close();
 
